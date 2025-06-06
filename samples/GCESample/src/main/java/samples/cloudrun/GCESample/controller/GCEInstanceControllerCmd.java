cc92478ff563cd9e6e7b4e3e612aa96dcd6e962c
Tim Payne <tim.tpayne@gmail.com>
2025-06-06 21:29:34 +0100

    Update samples/GCESample/src/main/java/samples/cloudrun/GCESample/controller/GCEInstanceControllerCmd.java on branch codeimprovements

--- samples/GCESample/src/main/java/samples/cloudrun/GCESample/controller/GCEInstanceControllerCmd.java
+++ samples/GCESample/src/main/java/samples/cloudrun/GCESample/controller/GCEInstanceControllerCmd.java
@@ -1,17 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package samples.cloudrun.GCESample.controller;
 
 import samples.cloudrun.GCESample.service.GCEInstanceService;
@@ -21,32 +7,13 @@ import samples.cloudrun.GCESample.model.cmd.deleteComputeInstanceParams;
 import samples.cloudrun.GCESample.cmd.GCEComputeCmd;
 import samples.cloudrun.GCESample.message.ResponseMessage;
 
-import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Collections;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-
 import java.util.logging.Logger;
-import java.util.logging.Level;
 
 @RestController
 @RequestMapping("/compute")
@@ -54,163 +21,60 @@ public class GCEInstanceControllerCmd {
 
     private final GCEInstanceService service = GCEInstanceService.getInstance();
 
-    // Shared logger for messages
     private static final Logger LOGGER = Logger.getLogger(GCEInstanceControllerCmd.class.getName());
-
-    // This is a potential security hole as the instance is shared, but makes start
-    // times quicker...
-    // Ideally, not an approach to use with unauthenticated exposed services, but
-    // okay for a demo...
     private static GCEComputeCmd cmd = new GCEComputeCmd();
 
-    // Version checker
     @GetMapping("/version")
     public String versionApp() {
         return String.format("<h2>Version 1.0</h2>");
     }
 
-    // Created instance cache lister
     @GetMapping("/list")
     public Map<String, Object> list() {
-        Map<String, Object> response = new HashMap<String, Object>();
+        Map<String, Object> response = new HashMap<>();
         response.put("List of created instances", service.getComputeInstances());
         return response;
     }
 
-    // URL mapper for getting details of a specific instance from a full URL
-    @GetMapping("/describe/{projectId:.+}/{zone:.+}/{instanceName:.+}")
-    public Map<String, Object> describeInstanceURL(@PathVariable String projectId,
-            @PathVariable String zone,
-            @PathVariable String instanceName) {
-        Map<String, Object> response = new HashMap<String, Object>();
-        response.put("Instance:", cmd.describeInstance(projectId, zone, instanceName));
-        return response;
-    }
-
-    // Get the details of a specific instance from query params specified to URL
     @GetMapping("/describe")
-    public Map<String, Object> describeInstance(@RequestParam String projectId,
-            @RequestParam String zone,
-            @RequestParam String instanceName) {
-        Map<String, Object> response = new HashMap<String, Object>();
+    public Map<String, Object> describeInstance(@RequestParam String projectId, @RequestParam String zone, @RequestParam String instanceName) {
+        Map<String, Object> response = new HashMap<>();
         response.put("Instance:", cmd.describeInstance(projectId, zone, instanceName));
         return response;
     }
 
-    // URL mapper for listing details of all instances for a project/zone from a
-    // full URL
-    @GetMapping("/listAll/{projectId:.+}/{zone:.+}")
-    public Map<String, List<Object>> listAllURL(@PathVariable String projectId,
-            @PathVariable String zone) {
-        Map<String, List<Object>> response = new HashMap<String, List<Object>>();
-        response.put("List of all instances", cmd.listInstances(projectId, zone));
-        return response;
-    }
-
-    // URL mapper for listing details of all instances for a project from a full URL
-    @GetMapping("/listAll/{projectId:.+}")
-    public Map<String, Map<String, List<Object>>> listAllURL(@PathVariable String projectId) {
-        Map<String, Map<String, List<Object>>> response = new HashMap<String, Map<String, List<Object>>>();
-        response.put("List of all instances", cmd.listInstances(projectId));
-        return response;
-    }
-
-    // List details of all instances for a project/zone provided by query params
-    // specified to URL
-    @GetMapping("/listAll")
-    public Map<String, List<Object>> listAll(@RequestParam String projectId,
-            @RequestParam String zone) {
-        Map<String, List<Object>> response = new HashMap<String, List<Object>>();
-        response.put("List of all instances", cmd.listInstances(projectId, zone));
-        return response;
-    }
-
-    // URL mapper for deleting an instance specified by a full URL
-    @DeleteMapping("/{projectId:.+}/{zone:.+}/{instanceName:.+}")
-    public ResponseEntity<ResponseMessage> deleteInstance(@PathVariable String projectId,
-            @PathVariable String zone,
-            @PathVariable String instanceName) {
-
-        String message = "";
-        try {
-            // Instantiates a client
-            GCEInstance instance = new GCEInstance(instanceName,
-                    zone);
-
-            if (!cmd.deleteInstance(projectId, instance)) {
-                message = "Instance deletion failed";
-                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-            }
-
-            service.getComputeInstances().remove(instance.getInstanceName());
-            LOGGER.info("deleteInstance() : Instance deleted");
-            message = "Instance deleted successfully";
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception e) {
-            LOGGER.severe("deleteInstance() :" + e.getMessage());
-            message = "Instance deleted failed with error: " + e.getMessage();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-        }
-    }
-
-    // Naught usage - not standard!
-    // Post mapper for JSON slurper to delete instance details based on JSON text
-    @PostMapping(path = "/delete", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseMessage> deleteInstance(
-            @RequestHeader(name = "X-COM-PERSIST", required = false) String headerPersist,
-            @RequestHeader(name = "X-COM-LOCATION", defaultValue = "USA") String headerLocation,
-            @RequestBody deleteComputeInstanceParams params) {
-
-        String message = "";
-        try {
-            // Instantiates a client
-            GCEInstance instance = new GCEInstance(params.getInstanceName(),
-                    params.getZone());
-
-            if (!cmd.deleteInstance(params.getProjectId(), instance)) {
-                message = "Instance deletion failed";
-                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-            }
-
-            service.getComputeInstances().remove(instance.getInstanceName());
-            LOGGER.info("deleteInstance() : Instance deleted");
-            message = "Instance deleted successfully";
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception e) {
-            LOGGER.severe("deleteInstance() :" + e.getMessage());
-            message = "Instance deleted failed with error: " + e.getMessage();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-        }
-    }
-
-    // Post mapper for JSON slurper to create instance details based on JSON text
     @PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseMessage> createInstance(
-            @RequestHeader(name = "X-COM-PERSIST", required = false) String headerPersist,
-            @RequestHeader(name = "X-COM-LOCATION", defaultValue = "USA") String headerLocation,
-            @RequestBody createComputeInstanceParams params) {
-
-        String message = "";
+    public ResponseEntity<ResponseMessage> createInstance(@RequestBody createComputeInstanceParams params) {
+        String message;
         try {
-            GCEInstance gceins = new GCEInstance(params.getInstanceName(),
-                    params.getZone(),
-                    params.getImageSource(),
-                    params.getNetworkInterface(),
-                    params.getNetworkConfig(),
-                    params.getMachineType());
-            LOGGER.info("createInstance() :" + params.getProjectId() + " " + gceins.toString());
+            GCEInstance gceins = new GCEInstance(params.getInstanceName(), params.getZone(), params.getImageSource(), params.getNetworkInterface(), params.getNetworkConfig(), params.getMachineType());
             if (!cmd.createInstance(params.getProjectId(), gceins)) {
                 message = "Instance creation failed";
                 return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
             }
-
-            service.getComputeInstances().put(gceins.getInstanceName(), gceins);
             LOGGER.info("createInstance() : Instance created");
             message = "Instance created successfully";
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
         } catch (Exception e) {
-            LOGGER.severe("createInstance() :" + e.getMessage());
             message = "Instance creation failed with error: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @DeleteMapping("/{projectId:.+}/{zone:.+}/{instanceName:.+}")
+    public ResponseEntity<ResponseMessage> deleteInstance(@PathVariable String projectId, @PathVariable String zone, @PathVariable String instanceName) {
+        String message;
+        try {
+            GCEInstance instance = new GCEInstance(instanceName, zone);
+            if (!cmd.deleteInstance(projectId, instance)) {
+                message = "Instance deletion failed";
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+            LOGGER.info("deleteInstance() : Instance deleted");
+            message = "Instance deleted successfully";
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Instance deletion failed with error: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
     }
